bdc093385a54334684fe8847d648cd40a92dcaaf
Rahul Ravindran <rahul.r945@gmail.com>
2013-10-13 17:48:50 +0000

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-var crossBrowser = (navigator.getUserMedia || navigator.webkitGetUseMedia || navigator.mozGetUserMedia );
-if(crossBrowser){
-navigator.getUserMedia({audio :false, video:true }, function(stream){
