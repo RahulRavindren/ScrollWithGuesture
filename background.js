2cc54433d6edfdc086b15a1fe34f4c6137520f81
SupportBee <rahul.r945@gmail.com>
2013-11-17 02:39:42 +0000

    added calculating function

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ start();
 
 function start(){
 	hasGetUserMedia();
-if(navigator.getUserMedia){
+if(navigator.getUserMedia){(
 navigator.getUserMedia({audio:false , video:false} ,function(stream){
 this.source = stream;
 update();
@@ -33,13 +33,15 @@ checkAreas();
 
 var canvasSource= $('#canvas-source')[0];
 var canvasBlend= $('#canvas-blend') [0];
-var
+var sourceContext = canvasSource.getContext('2d');
+var blendContext = canvasBlend.getContext('2d');
 function blend(){
 var width= canvasSource.width;
 var height= canvasSource.height;
 	var webcamImage = canvasSource.getImageData(0,0,width,height);
 	if(!nextImage) var nextImage = canvasSource.getImage(0,0,width,height);
-
-
+	var blendImage = canvasSource.createImage(width,height);
+	differenceAccuracy(blendImage.data,webcamImage.data,nextImage.data);
+	blendContext.addImage(blendImage.data,0,0);
 
 }
